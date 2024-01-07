30c6ac7f6a2599dfab77a2f95d3b6dd21260cfe8
Eunjae Lee <vanitasium@gmail.com>
2024-01-08 00:56:58 +0900

    NestJS

---
+++ messages/src/messages/messages.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDTO } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(
+    public messagesService: MessagesService,
+    public messagesService2: MessagesService,
+    public messagesService3: MessagesService,
+  ) {
+    console.log(messagesService === messagesService2);
+    console.log(messagesService2 === messagesService3);
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDTO) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
+  }
+}
---
+++ scratch/README.md
@@ -0,0 +1,14 @@
+# Dependencies
+
+```sh
+npm i install @nestjs/common@7.6.17 @nestjs/core@7.6.17 @nestjs/platform-express@7.6.17 @reflect-metadata@0.1.13 @typescript@4.3.2
+```
+
+1. @nestjs/common, @nestjs/core
+   - contains vast majority of functions, classes, etc we need from Nest
+2. @nestjs/platform-express
+   - Let Nest use ExpressJS for handling HTTP requests
+3. reflect-metadata
+   - Helps make decorators work
+4. typescript
+   - write Nest apps with TS

12ff6e4a85e4a9c0a678f0316439ecd71c203acb
Eunjae Lee <vanitasium@gmail.com>
2024-01-08 19:02:59 +0900

    NestJS MyCV

--- di/src/disk/disk.module.ts
+++ di/src/disk/disk.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DiskService } from './disk.service';
 import { PowerModule } from 'src/power/power.module';
+import { PowerService } from 'src/power/power.service';
 
 @Module({
   imports: [PowerModule],
